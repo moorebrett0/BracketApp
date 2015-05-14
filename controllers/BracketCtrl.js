f895f0256c564184381a83957e2aa28be1330f15
Brett Moore & Tom Mertz <student@epicodus.com>
2015-05-14 10:37:25 -0700

    add custom filter function

--- controllers/BracketCtrl.js
+++ controllers/BracketCtrl.js
@@ -2,4 +2,18 @@ bracketApp.controller('BracketCtrl', function BracketCtrl($scope, PlayersFactory
     $scope.PlayersFactory = PlayersFactory;
     $scope.players = PlayersFactory.players;
 
+
+
+    $scope.oneOrMore = function (value, index) {
+        if (value.wins >= 1) {
+            return true;
+        }
+    }
+
+    $scope.twoOrMore = function(value, index) {
+        if (value.wins >= 2) {
+            return true;
+        }
+    }
+
 });
