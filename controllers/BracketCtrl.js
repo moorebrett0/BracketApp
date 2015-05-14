2ae7869cc693c07e31133371c6ac1d36b3835463
Brett Moore & Tom Mertz <student@epicodus.com>
2015-05-14 10:54:33 -0700

    add resetWins function, completed styling

--- controllers/BracketCtrl.js
+++ controllers/BracketCtrl.js
@@ -8,12 +8,18 @@ bracketApp.controller('BracketCtrl', function BracketCtrl($scope, PlayersFactory
         if (value.wins >= 1) {
             return true;
         }
-    }
+    };
 
     $scope.twoOrMore = function(value, index) {
         if (value.wins >= 2) {
             return true;
         }
-    }
+    };
+
+    $scope.resetWins = function() {
+        $scope.players.forEach(function(element) {
+            element.wins = 0;
+        });
+    };
 
 });
